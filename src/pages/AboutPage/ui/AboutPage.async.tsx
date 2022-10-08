bc2bce06a9bb5ed1e50072e1a7635d24eb3a01d5
Denisphuket <taxiphuket@yandex.ru>
2022-10-08 13:36:42 +0200

    08.10 - add main yml try fix 7

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -3,5 +3,5 @@ import { lazy } from 'react';
 // export const AboutPageAsync = lazy(() => import('./AboutPage'));
 export const AboutPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
-    setTimeout(() => resolve(import('./aboutPage')), 1500);
+    setTimeout(() => resolve(import('./AboutPage')), 1500);
 }));
