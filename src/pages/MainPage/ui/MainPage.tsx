c2fbc1259c95133fe3ca7328f3e58135dc82c99e
Denisphuket <taxiphuket@yandex.ru>
2022-10-16 12:08:21 +0200

    16.10 - add redux, first entities

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
 
 function MainPage() {
     const { t } = useTranslation('MainPage');
@@ -10,7 +9,6 @@ function MainPage() {
         <div>
             <BugButton />
             {t('Главная')}
-            <Counter />
         </div>
     );
 }
